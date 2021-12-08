b0dd91f2e4d318f05cebccb6c499a2e21d05f5f6
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-08 21:01:05 +0700

    config laravel spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             PermissionSeeder::class,
             RoleSyncPermissions::class,
+            UserSeeder::class,
         ]);
     }
 }
