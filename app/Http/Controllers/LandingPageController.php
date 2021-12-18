d52a37d62046af1d7e646f65fc8f42a5805afd46
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-18 20:28:06 +0700

    update

---
+++ app/Http/Controllers/LandingPageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class LandingPageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $categories = Category::with('products')->get();
+
+        return view('landingPage', compact('categories'));
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 
     protected $guarded = ['id'];
 
-    public function FunctionName()
+    public function category()
     {
         return $this->belongsTo(Category::class);
     }
