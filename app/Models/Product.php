63d0fd652e911188d40f29f941e30c37e4d9cdb8
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-24 23:56:11 +0700

    new model : ProductReview

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,4 +20,9 @@ class Product extends Model
     {
         return $this->hasMany(ProductImage::class);
     }
+
+    public function reviews()
+    {
+        return $this->hasMany(ProductReview::class);
+    }
 }
