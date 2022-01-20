40a273f74a48d6fc737633f1598cb34190ae9926
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-20 20:21:03 +0700

    Update BannerSeeder.php

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -17,17 +17,17 @@ class BannerSeeder extends Seeder
         Banner::create([
             'image' => 'banner1.webp',
             'path' => 'images/banners/',
-            'description' => ''
+            'description' => 'Dui efficitur fames purus non himenaeos congue nec. Elementum a porttitor laoreet nam rutrum platea neque condimentum enim. Netus risus rutrum purus sapien accumsan nam quisque cubilia.'
         ]);
         Banner::create([
             'image' => 'banner2.webp',
             'path' => 'images/banners/',
-            'description' => ''
+            'description' => 'Dui efficitur fames purus non himenaeos congue nec. Elementum a porttitor laoreet nam rutrum platea neque condimentum enim. Netus risus rutrum purus sapien accumsan nam quisque cubilia.'
         ]);
         Banner::create([
             'image' => 'banner3.webp',
             'path' => 'images/banners/',
-            'description' => ''
+            'description' => 'Dui efficitur fames purus non himenaeos congue nec. Elementum a porttitor laoreet nam rutrum platea neque condimentum enim. Netus risus rutrum purus sapien accumsan nam quisque cubilia.'
         ]);
     }
 }
